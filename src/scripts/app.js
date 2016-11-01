c7087bd7b74d1752c99f14fe40edb2918ac24a2b
Paul Swift <paulswift@Pauls-MacBook.local>
2016-11-01 16:20:23 -0400

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,51 @@
-const Backbone = require('Backbone')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const $ = require('jquery')
 
-console.log(Backbone)
-document.querySelector('#app-container').innerHTML = `<h1>YOLO</h1>`
+console.log(React)
+
+let HomeView = React.createClass({
+   render: function(){
+
+   let jsxArray = this.props.userDataList.map(function(userObj, i){
+         return < CongressPeople userDataList={userObj} key={i} />
+      })
+      return (
+         <div>
+            <ul>
+               {jsxArray}
+            </ul>
+         </div>
+      )
+   }
+})
+
+
+
+let CongressPeople = React.createClass({
+   render:function(){
+
+      return(
+              <div className="col-sm-4 col-md-4">
+                   <div className="thumbnail green">
+                     <div className="caption">
+                       <h3>  {this.props.userDataList.first_name} {this.props.userDataList.last_name}  </h3>
+                       <p>  {this.props.userDataList.title}--{this.props.userDataList.party}-{this.props.userDataList.state_name}  </p>
+                       <ul>
+                          <li>  {this.props.userDataList.oc_email}  </li>
+                          <li>  {this.props.userDataList.fec_ids}  </li>
+                          <li>  {this.props.userDataList.twitter_id}  </li>
+                       </ul>
+                       <p>   Term End{this.props.userDataList.term_end} </p>
+                     </div>
+                   </div>
+                 </div>
+
+       )
+
+   }
+})
+
+$.getJSON('http://congress.api.sunlightfoundation.com/legislators?apikey=0156daaa29b04cfeb56537b9329ba35d').then(function(serverRes){
+   ReactDOM.render(<HomeView userDataList={serverRes.results}/>, document.querySelector('#app-container'))
+})
